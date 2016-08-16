3cf26a37eb20970644a4b7edcd784563cd9aec9b
James Wang <james.k.wang.18@dartmouth.edu>
2016-08-16 19:33:14 -0400

    final

--- src/components/app.js
+++ src/components/app.js
@@ -1,3 +1,6 @@
+// at top
+import io from 'socket.io-client';
+const socketserver = 'http://localhost:9090';
 import React, { Component } from 'react';
 import Immutable from 'immutable';
 import Note from './note';
@@ -14,6 +17,11 @@ class App extends Component {
     this.createNote = this.createNote.bind(this);
     this.renderNotes = this.renderNotes.bind(this);
     this.updateNotes = this.updateNotes.bind(this);
+    this.socket = io(socketserver);
+    this.socket.on('connect', () => { console.log('socket.io connected'); });
+    this.socket.on('disconnect', () => { console.log('socket.io disconnected'); });
+    this.socket.on('reconnect', () => { console.log('socket.io reconnected'); });
+    this.socket.on('error', (error) => { console.log(error); });
   }
 
   createNote(title) {
@@ -33,7 +41,7 @@ class App extends Component {
   renderNotes() {
     if (this.state.notes.size > 0) {
       return this.state.notes.entrySeq().map(([key, value]) =>
-        <Note note={value} id={key} onUpdate={this.updateNotes}
+        <Note note={value} id={key} key={key} onUpdate={this.updateNotes}
           onDelete={(id) => { this.setState({ notes: this.state.notes.delete(id) }); }}
         />);
     } else {
